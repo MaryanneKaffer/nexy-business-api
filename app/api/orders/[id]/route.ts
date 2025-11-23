ef22bcd16ebb747fa11f88a8230fea8cf3799ee8
maryanne <kaffermaryanne@gmail.com>
2025-11-23 18:13:39 -0300

    fix

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,9 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(req: Request, { params }: { params: { id: string } }) {
     const { id } = params;
 
     try {
